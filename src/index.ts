88e1ad0440c87ddb0ec807c17640b803ad41fdf9
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-05-05 14:11:50 +0200

    Add support for comparing primitives

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export default () => {};
+import { expect } from 'chai';
+
+interface Assertion<T> {
+  to: {
+    equal: (expected: T) => void;
+  }
+}
+
+function typedExpect<T>(actual: T): Assertion<T> {
+  return {
+    to: {
+      equal: (expected: T) => {
+        expect(actual).to.equal(expected);
+      }
+    }
+  };
+}
+
+export default typedExpect;
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,21 @@
 import typedExpect from 'src/index';
+import { expect } from 'chai';
 
-describe('Index', function () {
-  it('should work', function () {
-    typedExpect();
+describe('TypedExpect', function () {
+  describe('comparing primitives', function() {
+    it('numbers', function () {
+      typedExpect(3).to.equal(3);
+      expect(() => typedExpect(3).to.equal(4)).to.throw;
+    });
+
+    it('strings', function () {
+      typedExpect('foo').to.equal('foo');
+      expect(() => typedExpect('foo').to.equal('bar')).to.throw;
+    });
+
+    it('booleans', function () {
+      typedExpect(true).to.equal(true);
+      expect(() => typedExpect(true).to.equal(false)).to.throw;
+    });
   });
 });
