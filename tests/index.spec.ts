d3f78d2858e2682d55a260580a6c0b306fa0931f
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-05-05 22:03:41 +0200

    Reorganize test by method and then by type

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -2,7 +2,7 @@ import typedExpect from 'src/index';
 import { expect } from 'chai';
 
 describe('TypedExpect', function () {
-  describe('comparing primitives', function() {
+  describe('equal', function() {
     it('numbers', function () {
       typedExpect(3).to.equal(3);
       expect(() => typedExpect(3).to.equal(4)).to.throw();
@@ -17,23 +17,22 @@ describe('TypedExpect', function () {
       typedExpect(true).to.equal(true);
       expect(() => typedExpect(true).to.equal(false)).to.throw();
     });
-  });
 
-  describe('arrays', function() {
-    it('equals', function () {
+    it('arrays', function () {
       typedExpect([1, 2, 3]).to.equal([1, 2, 3]);
       expect(() => typedExpect([1, 2]).to.equal([2, 3])).to.throw();
     });
+  });
 
-    it('contains', function () {
+  describe('contains', function() {
+    it('arrays', function () {
       typedExpect([1, 2, 3]).to.contain(1);
       expect(() => typedExpect([1, 2]).to.contain(3)).to.throw();
     });
-  });
 
-  describe('strings', function () {
-    it('contains', function () {
+    it('strings', function () {
       typedExpect('foobar').to.contain('foo');
+      expect(() => typedExpect('foobar').to.contain('xxx')).to.throw();
     });
   });
 });
