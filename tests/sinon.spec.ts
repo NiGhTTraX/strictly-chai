080d2118a0c29b995259fb013c18e5fbb3db8b5e
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-05-09 23:03:44 +0200

    Make sure that the sinon plugin passes the contract of typedExpect

--- tests/sinon.spec.ts
+++ tests/sinon.spec.ts
@@ -1,14 +1,16 @@
 import enhance from 'src/enhance';
 import sinonPlugin from 'src/sinon';
 import { spy } from 'sinon';
+import contractTests from './expect-contract';
 
 describe('Sinon plugin', function () {
   const sinonExpect = enhance(sinonPlugin);
+
+  contractTests(sinonExpect);
 
   it('called', function () {
     const appleSpie = spy();
     appleSpie();
     sinonExpect(appleSpie).called();
-    sinonExpect(2).to.equal(2);
   });
 });
