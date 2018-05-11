7edefba3b3818aabfa8b2f9e445c8a21ba422a8a
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-05-11 19:12:09 +0200

    Improve type error detection tests

---
+++ tests/expect-type-errors.ts
@@ -0,0 +1,70 @@
+import * as fs from 'fs';
+import * as ts from 'typescript';
+import { expect } from 'chai';
+
+const tsConfig = require('../tsconfig.json');
+
+const compilerOptions = ts.convertCompilerOptionsFromJson(tsConfig.compilerOptions, '.').options;
+
+interface CompilationError {
+  line: number;
+  column: number;
+  message: string;
+}
+
+/**
+ * Expect that every line in the given file that has an expect call will
+ * result in a type incompatibility compiler error. It also expects that no
+ * other type of errors will be thrown.
+ *
+ * This assumes there won't be more than one call per line. The calls are
+ * identified by searching for a method call followed immediately by a property
+ * access.
+ */
+export default function expectTypeErrors(fileName: string) {
+  const errors = compile(fileName, compilerOptions);
+  const lines = fs.readFileSync(fileName, { encoding: 'utf-8' }).split('\n');
+
+  const errorsByLine = errors.reduce((acc: Map<number, boolean>, { line, column, message }) => {
+    expect(message).to.match(
+      /not assignable/,
+      `Non type error detected on ${fileName}:${line},${column}`
+    );
+
+    expect(acc.get(line), `More than 1 error detected on ${fileName}:${line} (${lines[line]})`)
+      .to.be.undefined;
+
+    acc.set(line, true);
+    return acc;
+  }, new Map());
+
+  lines.forEach((line, i) => {
+    if (line.indexOf(').') !== -1) {
+      expect(errorsByLine.get(i + 1), `No error detected on ${fileName}:${i + 1} (${line})`)
+        .to.not.be.undefined;
+    }
+  });
+}
+
+function compile(fileName: string, options: ts.CompilerOptions): CompilationError[] {
+  const program = ts.createProgram([fileName], options);
+
+  const emitResult = program.emit();
+  const allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
+
+  return allDiagnostics.map(diagnostic => {
+    const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+
+    if (diagnostic.file) {
+      const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start!);
+
+      return {
+        line: line + 1,
+        column: character + 1,
+        message
+      };
+    }
+
+    throw new Error(message);
+  });
+}
--- tests/expect-types.spec.ts
+++ tests/expect-types.spec.ts
@@ -1,74 +1,14 @@
-import * as ts from 'typescript';
-import { expect } from 'chai';
-import * as fs from 'fs';
+import expectTypeErrors from './expect-type-errors';
 
-const tsConfig = require('../tsconfig.json');
-
-const compilerOptions = ts.convertCompilerOptionsFromJson(tsConfig.compilerOptions, '.').options;
-
-interface CompilationError {
-  line: number;
-  column: number;
-  message: string;
-}
-
-function compile(fileName: string, options: ts.CompilerOptions): CompilationError[] {
-  const program = ts.createProgram([fileName], options);
-
-  const emitResult = program.emit();
-  const allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
-
-  return allDiagnostics.map(diagnostic => {
-    const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
-
-    if (diagnostic.file) {
-      const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start!);
-
-      return {
-        line: line + 1,
-        column: character + 1,
-        message
-      };
-    }
-
-    throw new Error(message);
-  });
-}
-
-describe('Types', function () {
+describe('Expect Types', function () {
   this.timeout(5 * 1000);
 
   it('should not allow to compare different types', function () {
-    expectErrors('tests/types/equal.ts');
+    expectTypeErrors('tests/types/equal.ts');
   });
 
   it('should not allow to check for members of different types', function () {
-    expectErrors('tests/types/contain.ts');
+    expectTypeErrors('tests/types/contain.ts');
   });
-
-  /**
-   * Expect that every line in the given file that has an expect call will
-   * result in a type incompatibility compiler error. It also expects that no
-   * other type of errors will be thrown.
-   */
-  function expectErrors(fileName: string) {
-    const errors = compile(fileName, compilerOptions);
-    const numberOfCalls = fs.readFileSync(fileName, { encoding: 'utf-8' })
-      .split('\n')
-      // Get the lines that have a method call followed by a property access.
-      .filter(line => line.indexOf(').') !== -1)
-      .length;
-
-    errors.forEach(({ line, column, message }) => {
-      expect(message).to.match(
-        /not assignable/,
-        `Error in ${fileName}:${line},${column} is not a type error`
-      );
-    });
-
-    expect(errors).to.have.length(
-      numberOfCalls,
-      errors.map(error => error.message).join('\n')
-    );
-  }
 });
+
