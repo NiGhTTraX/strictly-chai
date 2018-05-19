f1d9a0ce1083152f709701b1ba3663f490ccb8b1
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-05-19 11:12:14 +0200

    Increase timeout of sinon types tests

--- tests/sinon-types.spec.ts
+++ tests/sinon-types.spec.ts
@@ -1,6 +1,8 @@
 import expectTypeErrors from './expect-type-errors';
 
 describe('Sinon Types', function () {
+  this.timeout(5 * 1000);
+
   it('should be assignable to typedExpect', function () {
     expectTypeErrors('tests/types/sinon.ts');
   });
