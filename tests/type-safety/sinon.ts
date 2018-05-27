28dca4121f8912602048f83f1bee317939c376d3
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-05-27 15:49:53 +0200

    Remove Sinon plugin
    Moved to [strictly-sinon-chai](https://github
    .com/NiGhTTraX/strictly-sinon-chai).

--- tests/type-safety/sinon.ts
+++
@@ -1,6 +0,0 @@
-import { spy } from 'sinon';
-import sinonExpect from '../../src/sinon';
-
-const appleSpie = spy();
-sinonExpect(appleSpie).to.have.been.called(true);
-sinonExpect(appleSpie).to.have.been.calledWith();
